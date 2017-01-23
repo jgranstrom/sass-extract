90c5797d6d607b0ab7f5a4deff462781d2d15046
John Granström <granstrom.john@gmail.com>
2017-01-23 22:29:02 +0100

    Refactor to add async API variant

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const { renderSync } = require('./lib/render');
-const { extractSync } = require('./lib/extract');
+const { render, renderSync } = require('./lib/render');
+const { extract, extractSync } = require('./lib/extract');
 
+exports.render = render;
 exports.renderSync = renderSync;
+exports.extract = extract;
 exports.extractSync = extractSync;
 
 const res = exports.renderSync({
   file: require('path').join(__dirname, 'test.scss'),
 });
 
-console.log('vars', res.vars);
+
+console.log('vars sync', res.vars);
+
+exports.render({
+  file: require('path').join(__dirname, 'test.scss'),
+})
+.then(res => {
+  console.log('vars async', res.vars);
+}).done();
+
 
 setInterval(() => {
 
