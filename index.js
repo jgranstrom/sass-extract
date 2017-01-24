de9dbaaf00e3f989166c08b78bf2035af9d13e06
John Granström <granstrom.john@gmail.com>
2017-01-24 15:36:50 +0100

    Remove debug

--- index.js
+++ index.js
@@ -5,22 +5,3 @@ exports.render = render;
 exports.renderSync = renderSync;
 exports.extract = extract;
 exports.extractSync = extractSync;
-
-const res = exports.renderSync({
-  file: require('path').join(__dirname, 'test.scss'),
-});
-
-
-console.log('vars sync', res.vars);
-
-exports.render({
-  file: require('path').join(__dirname, 'test.scss'),
-})
-.then(res => {
-  console.log('vars async', res.vars);
-}).done();
-
-
-setInterval(() => {
-
-}, 1000);
