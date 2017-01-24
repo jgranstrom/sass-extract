c05685e5698adfa5c8b93c55ad65ffc66e69eb56
John Granström <granstrom.john@gmail.com>
2017-01-24 15:52:35 +0100

    Add support for comments

---
+++ test/comments.js
@@ -0,0 +1,59 @@
+const { expect } = require('chai');
+const path = require('path');
+const { render, renderSync } = require('../');
+
+const commentFile = path.join(__dirname, 'sass', 'comments.scss');
+
+function verifyComment(rendered, sourceFile) {
+  expect(rendered.vars).to.exist;
+  expect(rendered.vars).to.have.property('global');
+  expect(rendered.vars.global).to.have.property('$number1');
+  expect(rendered.vars.global).to.have.property('$number2');
+  expect(rendered.vars.global).to.have.property('$color');
+  expect(Object.keys(rendered.vars.global)).to.have.length(3);
+
+  expect(rendered.vars.global.$number1.value).to.equal(100);
+  expect(rendered.vars.global.$number1.unit).to.equal('px');
+  expect(rendered.vars.global.$number1.type).to.equal('SassNumber');
+  expect(rendered.vars.global.$number1.sources).to.have.length(1);
+  expect(rendered.vars.global.$number1.sources[0]).to.equal(sourceFile);
+  expect(rendered.vars.global.$number1.expressions).to.have.length(1);
+  expect(rendered.vars.global.$number1.expressions[0]).to.equal('100px');
+
+  expect(rendered.vars.global.$number2.value).to.equal(200);
+  expect(rendered.vars.global.$number2.unit).to.equal('px');
+  expect(rendered.vars.global.$number2.type).to.equal('SassNumber');
+  expect(rendered.vars.global.$number2.sources).to.have.length(1);
+  expect(rendered.vars.global.$number2.sources[0]).to.equal(sourceFile);
+  expect(rendered.vars.global.$number1.expressions).to.have.length(1);
+  expect(rendered.vars.global.$number2.expressions[0]).to.equal('$number1 * 2');
+
+  expect(rendered.vars.global.$color.value.r).to.equal(255);
+  expect(rendered.vars.global.$color.value.g).to.equal(0);
+  expect(rendered.vars.global.$color.value.b).to.equal(0);
+  expect(rendered.vars.global.$color.value.a).to.equal(1);
+  expect(rendered.vars.global.$color.value.hex).to.equal('#ff0000');
+  expect(rendered.vars.global.$color.type).to.equal('SassColor');
+  expect(rendered.vars.global.$color.sources).to.have.length(1);
+  expect(rendered.vars.global.$color.sources[0]).to.equal(sourceFile);
+  expect(rendered.vars.global.$color.expressions).to.have.length(1);
+  expect(rendered.vars.global.$color.expressions[0]).to.equal('red');
+}
+
+describe('comments', () => {
+  describe('sync', () => {
+    it('should extract variables not in comments', () => {
+      const rendered = renderSync({ file: commentFile })
+      verifyComment(rendered, commentFile);
+    });
+  });
+
+  describe('async', () => {
+    it('should extract variables not in comments', () => {
+      return render({ file: commentFile })
+      .then(rendered => {
+        verifyComment(rendered, commentFile);
+      });
+    });
+  });
+});
