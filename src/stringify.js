bb8e5605f6705b7da96a7cfe37e797c758e03252
John Granström <granstrom.john@gmail.com>
2017-11-29 18:59:15 +0100

    refactor(deps): remove lodash from stringify

--- src/stringify.js
+++ src/stringify.js
@@ -9,10 +9,6 @@ Neither the name of salesforce.com, inc. nor the names of its contributors may b
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-/* eslint-disable camelcase */
-
-const _ = require('lodash')
-
 let type = {
   arguments: (n) =>
     '(' + walkValue(n.content) + ')',
@@ -48,13 +44,13 @@ let type = {
 
 let walkNode = (node) => {
   if (type[node.type]) return type[node.type](node)
-  if (_.isString(node.content)) return node.content
-  if (_.isArray(node.content)) return walkValue(node.content)
+  if (typeof node.content === 'string') return node.content
+  if (Array.isArray(node.content)) return walkValue(node.content)
   return ''
 }
 
 let walkValue = (value) => {
-  if (!_.isArray(value)) return ''
+  if (!Array.isArray(value)) return ''
   return value.reduce((s, node) => {
     return s + walkNode(node)
   }, '')
