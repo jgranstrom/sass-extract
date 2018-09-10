9b7ca785655088d3c786230bca47894c009f6bf2
bencergazda <benedek.kaldi@srg.hu>
2018-09-10 15:35:33 +0200

    fix(importer): should work also with single importer
    This has been removed by accident from the original code.

--- src/importer.js
+++ src/importer.js
@@ -94,6 +94,10 @@ export function makeImporter(extractions, includedFiles, includedPaths, customIm
     try {
       const promises = [];
       if (customImporter) {
+        if (!Array.isArray(customImporter)) {
+          customImporter = [customImporter]
+        }
+
         customImporter.forEach(importer => {
           promises.push(new Promise(res => {
             importer(url, prev, (value) => res(value));
